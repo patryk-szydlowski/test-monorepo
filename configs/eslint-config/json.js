0752554c57b9f5f80f8b59557d8e222c54c0216e
Patryk Szydłowski <28259376+patryk-szydlowski@users.noreply.github.com>
2022-12-01 19:41:20 +0100

    sort json

--- configs/eslint-config/json.js
+++ configs/eslint-config/json.js
@@ -7,6 +7,50 @@ module.exports = defineConfig({
       parser: "jsonc-eslint-parser",
       plugins: ["jsonc"],
       extends: ["plugin:jsonc/recommended-with-json", "plugin:jsonc/prettier"],
+      rules: {
+        "jsonc/sort-keys": "error",
+      },
+    },
+    {
+      files: ["package.json"],
+      rules: {
+        "jsonc/sort-keys": [
+          "error",
+          {
+            order: [
+              "name",
+              "description",
+              "private",
+              "packageManager",
+              "engines",
+              "scripts",
+              "dependencies",
+              "devDependencies",
+            ],
+            pathPattern: "^$",
+          },
+          {
+            order: ["node", "pnpm", "npm", "yarn"],
+            pathPattern: "^engines$",
+          },
+          {
+            order: [
+              { keyPattern: "^dev.*$" },
+              { keyPattern: "^build.*$" },
+              { keyPattern: "^preview.*$" },
+              { keyPattern: "^lint.*$" },
+              { keyPattern: "^test.*$" },
+            ],
+            pathPattern: "^scripts$",
+          },
+          {
+            order: {
+              type: "asc",
+            },
+            pathPattern: ".*",
+          },
+        ],
+      },
     },
   ],
 });
