2e4f69f855abbc173f4095479fb2aec9c9df0735
Patryk Szydłowski <28259376+patryk-szydlowski@users.noreply.github.com>
2022-12-01 20:22:33 +0100

    add basic nx support

--- packages/ui-primitives/src/components/button.tsx
+++ packages/ui-primitives/src/components/button.tsx
@@ -1,7 +1,10 @@
 export function Button() {
+  // eslint-disable-next-line i18n-text/no-en --- disabled for now
+  const translateThis = "Click me";
+
   return (
-    <button>
-      <div>Click me</div>
+    <button type="button">
+      <div>{translateThis}</div>
     </button>
   );
 }
